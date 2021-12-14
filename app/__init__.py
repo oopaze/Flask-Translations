fbf13411c00651ec3814c429b377da92433dd515
José Pedro <pedroosd28@gmail.com>
2021-12-14 02:09:24 -0300

    feat: configuring babel for translations and migrate for manage DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_babel import Babel
+from flask_migrate import Migrate
+
+from .src.db import configure_db
 
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object('config.Development')
+
+    configure_db(app)
+
+    babel = Babel(app)
+    Migrate(app, app.db)
 
     return app
 
